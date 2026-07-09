08847dfb08456c6707f79f588177547e7d384677
Nishanth Jadav <nishanthjadav@gmail.com>
2026-07-09 13:14:49 -0400

    added vercel analytics

--- Frontend/political-trades-frontend/src/main.jsx
+++ Frontend/political-trades-frontend/src/main.jsx
@@ -1,5 +1,6 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { Analytics } from '@vercel/analytics/react'
 import './index.css'
 import App from './App.jsx'
 import { AuthProvider } from './contexts/AuthContext.jsx'
@@ -26,5 +27,6 @@ createRoot(document.getElementById('root')).render(
         )}
       </AuthProvider>
     )}
+    <Analytics />
   </StrictMode>,
 )
